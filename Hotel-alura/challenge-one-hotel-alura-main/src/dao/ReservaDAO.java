b076f1524555642932749191497aa72029568ccd
Rosiane Aparecida da Silva <rosivencendocomjesus@gmail.com>
2025-11-06 07:35:33 -0300

    Update ReservaDAO.java

--- Hotel-alura/challenge-one-hotel-alura-main/src/dao/ReservaDAO.java
+++ Hotel-alura/challenge-one-hotel-alura-main/src/dao/ReservaDAO.java
@@ -1,97 +1,24 @@
-package dao;
+public List<Reserva> buscar(int id){
+    String sql = "SELECT * FROM RESERVAS WHERE ID = ?";
+    List<Reserva> reservaList = new ArrayList<>();
 
-import modelo.Hospede;
-import modelo.Reserva;
+    try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+        preparedStatement.setInt(1, id);
 
-import java.math.BigDecimal;
-import java.sql.*;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
+        try (ResultSet resultSet = preparedStatement.executeQuery()) {
+            while (resultSet.next()){
+                Reserva reserva = new Reserva(); // ← Novo objeto a cada loop
+                reserva.setId(resultSet.getInt("ID"));
+                reserva.setDataEntrada(LocalDate.parse(resultSet.getString("DATA_ENTRADA")));
+                reserva.setDataSaida(LocalDate.parse(resultSet.getString("DATA_SAIDA")));
+                reserva.setValor(resultSet.getBigDecimal("VALOR"));
+                reserva.setFormaDePagamento(resultSet.getString("FORMA_PAGAMENTO"));
 
-public class ReservaDAO {
-    private Connection connection;
-
-    public ReservaDAO(Connection connection){
-        this.connection = connection;
-    }
-
-    public int salvar(Reserva reserva){
-        String sql = "INSERT INTO RESERVAS(DATA_ENTRADA, DATA_SAIDA, VALOR, FORMA_PAGAMENTO) " +
-                "VALUES (? , ? , ? , ?)";
-
-        try(PreparedStatement preparedStatement = connection.prepareStatement(sql,
-                Statement.RETURN_GENERATED_KEYS)){
-            preparedStatement.setString(1, reserva.getDataEntrada().toString());
-            preparedStatement.setString(2, reserva.getDataSaida().toString());
-            preparedStatement.setBigDecimal(3, reserva.getValor());
-            preparedStatement.setString(4, reserva.getFormaDePagamento());
-
-            preparedStatement.execute();
-
-            try (ResultSet resultSet = preparedStatement.getGeneratedKeys()){
-                while (resultSet.next()){
-                    reserva.setId(resultSet.getInt(1));
-                }
+                reservaList.add(reserva);
             }
-        }catch (SQLException e){
-            throw new RuntimeException(e);
         }
-        return reserva.getId();
+    }catch (SQLException e){
+        throw new RuntimeException(e);
     }
-
-    public List<Reserva> buscar(int id){
-        String sql = "SELECT * FROM RESERVAS WHERE ID = ?";
-        List<Reserva> reservaList = new ArrayList<Reserva>();
-        Reserva reserva = new Reserva();
-
-        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
-            preparedStatement.setInt(1, id);
-
-            preparedStatement.execute();
-
-            try (ResultSet resultSet = preparedStatement.getResultSet()) {
-                while (resultSet.next()){
-                    reserva.setId(resultSet.getInt(1));
-                    reserva.setDataEntrada(LocalDate.parse(resultSet.getString(2)));
-                    reserva.setDataSaida(LocalDate.parse(resultSet.getString(3)));
-                    reserva.setValor(BigDecimal.valueOf(resultSet.getDouble(4)));
-                    reserva.setFormaDePagamento(resultSet.getString(5));
-
-                    reservaList.add(reserva);
-                }
-            }
-        }catch (SQLException e){
-            throw new RuntimeException(e);
-        }
-        return reservaList;
-    }
-
-    public void deletar(int id){
-        String sql = "DELETE FROM RESERVAS WHERE ID = ?";
-
-        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
-            preparedStatement.setInt(1, id);
-            preparedStatement.execute();
-        }catch (SQLException e){
-            throw new RuntimeException(e);
-        }
-    }
-
-    public void alterar(Reserva reserva){
-        String sql = "UPDATE RESERVAS R SET R.DATA_ENTRADA = ? , R.DATA_SAIDA = ? , " +
-                "R.VALOR = ? , R.FORMA_PAGAMENTO = ? WHERE ID = ?";
-
-        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
-            preparedStatement.setString(1, reserva.getDataEntrada().toString());
-            preparedStatement.setString(2, reserva.getDataSaida().toString());
-            preparedStatement.setBigDecimal(3, reserva.getValor());
-            preparedStatement.setString(4, reserva.getFormaDePagamento());
-            preparedStatement.setInt(5, reserva.getId());
-
-            preparedStatement.execute();
-        }catch (SQLException e){
-            throw new RuntimeException(e);
-        }
-    }
+    return reservaList;
 }
