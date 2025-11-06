2abdf58b48ec715da5bf1b8e528f458704636b74
Rosiane Aparecida da Silva <rosivencendocomjesus@gmail.com>
2025-11-06 07:48:19 -0300

    Update ConnectionFactory.java

--- Hotel-alura/challenge-one-hotel-alura-main/src/factory/ConnectionFactory.java
+++ Hotel-alura/challenge-one-hotel-alura-main/src/factory/ConnectionFactory.java
@@ -7,26 +7,59 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-    public DataSource dataSource;
+    private DataSource dataSource;
 
-    public ConnectionFactory(){
-        //Criando um pool de conexões
-        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
-        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/hotel_alura?useTimezone=true&serverTimezone=UTC");
-        comboPooledDataSource.setUser("root");
-        comboPooledDataSource.setPassword("bd@Lucas95");
+    public ConnectionFactory() {
+        try {
+            ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+            
+            // Configuração do driver
+            comboPooledDataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            
+            // URL de conexão melhorada
+            comboPooledDataSource.setJdbcUrl(
+                "jdbc:mysql://localhost/hotel_alura?" +
+                "useTimezone=true&serverTimezone=UTC&" +
+                "useSSL=false&" +
+                "allowPublicKeyRetrieval=true&" +
+                "characterEncoding=utf8"
+            );
+            
+            comboPooledDataSource.setUser("root");
+            comboPooledDataSource.setPassword("bd@Lucas95");
 
-        comboPooledDataSource.setMaxPoolSize(10);
+            // Configurações do pool de conexões
+            comboPooledDataSource.setMinPoolSize(3);
+            comboPooledDataSource.setMaxPoolSize(15);
+            comboPooledDataSource.setAcquireIncrement(3);
+            comboPooledDataSource.setMaxIdleTime(300);
+            comboPooledDataSource.setCheckoutTimeout(5000);
+            comboPooledDataSource.setMaxConnectionAge(7200);
+            comboPooledDataSource.setIdleConnectionTestPeriod(300);
+            comboPooledDataSource.setTestConnectionOnCheckout(true);
+            
+            // Configurações de retry
+            comboPooledDataSource.setAcquireRetryAttempts(3);
+            comboPooledDataSource.setAcquireRetryDelay(1000);
 
-        //atribuindo o pool de conexões para a datasource
-        this.dataSource = comboPooledDataSource;
+            this.dataSource = comboPooledDataSource;
+            
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao configurar pool de conexões", e);
+        }
     }
-    public Connection recuperaConexao()  {
-        //recuperando a conexão já aberta pelo pool
+
+    public Connection recuperaConexao() {
         try {
             return this.dataSource.getConnection();
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Erro ao obter conexão do pool de conexões", e);
+        }
+    }
+    
+    public void fecharPool() {
+        if (this.dataSource instanceof ComboPooledDataSource) {
+            ((ComboPooledDataSource) this.dataSource).close();
         }
     }
 }
